c1d597ded3b32fe8313b9ed4f0cab01dc4e36a84
Chris Pak <chris.j.pak@stanford.edu>
2011-10-15 17:45:56 -0700

    finished the xy2d function, not going to add a d2xy function yet, don't know if I need it yet?

--- HilbertFunction.hs
+++ HilbertFunction.hs
@@ -7,3 +7,25 @@ module HilbertFunction where
 import Data.Word
 import Data.Bits
 
+xy2d :: Word32 -> Word32 -> Word32 -> Word32 
+xy2d size x y = xy2dr size (x, y) 0 where
+    xy2dr 0 (x, y) total = total
+    xy2dr s (x, y) total = xy2dr (s `div` 2) rot (total + inc) where
+        rx = (x .&. s) > 0
+        ry = (y .&. s) > 0
+        rot = rotater s (x,y) rx ry
+        inc = s * s * (number rx ry)
+
+number :: Bool -> Bool -> Word32
+number rx ry 
+    | rx && ry       = 2
+    | rx && (not ry) = 3
+    | (not rx) && ry = 1
+    | otherwise      = 0
+
+rotater :: Word32 -> (Word32, Word32) -> Bool -> Bool -> (Word32, Word32)
+rotater n (x,y) rx ry
+    | rx && (not ry) = (n - 1 - x, n - 1- y)
+    | (not ry)       = (y, x)
+    | otherwise      = (x, y)
+    
