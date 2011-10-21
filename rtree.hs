81c9ce8d0beb8320222033b915a8a19ab5e7969c
Chris Pak <chris.j.pak@stanford.edu>
2011-10-21 14:13:36 -0700

    finished search function, working now on main :)

--- rtree.hs
+++ rtree.hs
@@ -89,43 +89,8 @@ children (INode _ _ list) = concat $ map getNodes list
 
 --returns search list
 search :: TNode -> Rect -> [Rect]
-{-
---returns a list of rectangles that intersect query window.
-search :: HTree -> Rect -> [Rect]
-search tree rect
-    |(nType tree) == Empty  = error "trying to search in an empty list"
-    |(nType tree) == Object = [(mbr tree)]
-    |(nType tree) == Outer  = [mbr x | x <- ((fromJust . child) tree), (overlap . mbr) x]
-    |(nType tree) == Inner  = concatMap (flip search rect) children
-    where 
-        overlap = intersect rect
-        children = [x | x <-((fromJust . child) tree), (overlap . mbr) x]
-
--- creates a node to contain an actual rectangle 
-wrapRect :: Rect -> HTree
-wrapRect rect = Node { lhv   = hlbRect rect
-                     , child = Nothing
-                     , mbr   = rect
-                     , nType = Object
-                     }
-
-
-
---inserts a rectangle into the tree
-insert :: HTree -> Rect -> HTree
-insert root rect
-    |
-
--- find LHV from a given list of HTrees, presumably children
-getLHV :: [Node] -> Word32
-getLHV list = maximum [lhv x | x <- list]
-
--- fine the MBR for a given list of HTrees, again
--- presumably children
-getMBR :: [Node] -> MBR
-getMBR (a:[]) = mbr a
-getMBR (a:as) = createMBR (mbr a) (getMBR as)
--}
-
+search (LNode _ _ list) rect = filter (\r2 -> intersect rect r2) list
+search (INode _ _ list) rect = concat $ (map $ flip search rect) ( 
+    filter (\node -> intersect rect $ getMBR node) list ) 
 
 
